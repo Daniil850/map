c7ad34c75eea2dc5f0054b9d4e793ca3b7b8d9f9
Daniil850 <daniilzagorodny029@gmail.com>
2021-12-20 22:11:21 +0300

    end

--- maps_Zgorodny_Zyablov/LocationVC.swift
+++ maps_Zgorodny_Zyablov/LocationVC.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import CoreLocation
 
-class LocationVC: UIViewController {
-
+class LocationVC: UIViewController, CLLocationManagerDelegate {
+    let LocationManager = CLLocationManager()
+    @IBOutlet weak var dataLat: UILabel!
+    @IBOutlet weak var dataLon: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkAuthorization()
     }
-
-
+func checkAuthorization(){
+    switch CLLocationManager.authorizationStatus() {
+    case .authorizedWhenInUse:
+        LocationManager.delegate = self
+        LocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        LocationManager.startUpdatingLocation()
+        print("Error")
+    case .notDetermined:
+        LocationManager.requestWhenInUseAuthorization()
+        default:
+            break
 }
-
+}
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+        if let location = locations.last?.coordinate {
+            dataLat.text = String(location.latitude)
+            dataLon.text = String(location.longitude)
+            LocationManager.stopUpdatingLocation()
+        }
+        
+    }
+    
+}
